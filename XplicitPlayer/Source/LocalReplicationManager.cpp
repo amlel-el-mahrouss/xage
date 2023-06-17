ec52db647db1bea559d858a7873bee58180a4272
Amlal <amlal@xplicit.com>
2023-06-17 22:51:57 +0200

    XplicitPlayer: add LocalReplicationManager.cpp
    Signed-off-by: Amlal <amlal@xplicit.com>

---
+++ XplicitPlayer/Source/LocalReplicationManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================
+ *
+ *			XplicitNgin
+ *			Copyright Xplicit Corporation, all rights reserved.
+ *
+ *			File: LocalReplicationManager.h
+ *			Purpose: Local replication component.
+ *
+ * =====================================================================
+ */
+
+#include "LocalReplicationManager.h"
+
+namespace Xplicit
+{
+
+}
