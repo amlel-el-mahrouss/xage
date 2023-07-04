40686e2d6b97724ce0b0befc058e01e1ecdc56da
Amlal <amlal@play-xplicit.com>
2023-07-04 15:53:32 +0200

    Data/ fix lua API.
    Signed-off-by: Amlal <amlal@play-xplicit.com>

--- Data/Contents/xplicit.lua
+++ Data/Contents/xplicit.lua
@@ -1,156 +1,154 @@
 -- Copyright Xplicit Corporation
 
-PrintLn('XplicitNgine');
-
 # Copyright Xplicit Corporation
 
 _G.Vector3 = {}
 
 func _G.Vector3:New(X, Y, Z)
-    return { Name = "Vector3", Type = "Vector", X = X, Y = Y, Z = Z };
+    return { Name = "Vector3", Type = "Vector", X = X, Y = Y, Z = Z }
 end
 
 _G.Color = {}
 
 func _G.Color:New(R, G, B)
-    return { Name = "Color", Type = "Color", Red = R, Green = G, Blue = B };
+    return { Name = "Color", Type = "Color", Red = R, Green = G, Blue = B }
 end
 
 _G.HUMANOID = {}
 
-_G.HUMANOID.ALIVE = 0;
-_G.HUMANOID.DEAD = 1;
-_G.HUMANOID.INVALID = 3;
+_G.HUMANOID.ALIVE = 0
+_G.HUMANOID.DEAD = 1
+_G.HUMANOID.INVALID = 3
 
-Engine.Events = {};
+_G.Engine.Events = {}
 
-Engine.Counter = 0;
-Engine.PlayerCount = 0;
+_G.Engine.Counter = 0
+_G.Engine.PlayerCount = 0
 
-func Engine:Join()
-    Engine.PlayerCount = Engine.PlayerCount + 1;
+func _G.Engine:Join()
+    _G.Engine.PlayerCount = _G.Engine.PlayerCount + 1
 
-    for _, v in ipairs(Engine.Events) do
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "Join") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:Leave()
-    Engine.PlayerCount = Engine.PlayerCount - 1;
+func _G.Engine:Leave()
+    _G.Engine.PlayerCount = _G.Engine.PlayerCount - 1
 
-    for _, v in ipairs(Engine.Events) do
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "Leave") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:LeftClick()
-    for _, v in ipairs(Engine.Events) do
+func _G.Engine:LeftClick()
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "LeftClick") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:RightClick()
-    for _, v in ipairs(Engine.Events) do
+func _G.Engine:RightClick()
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "RightClick") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:MouseMove()
-    for _, v in ipairs(Engine.Events) do
+func _G.Engine:MouseMove()
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "MouseMove") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:LocalSpawn()
-    for _, v in ipairs(Engine.Events) do
+func _G.Engine:LocalSpawn()
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "LocalSpawn") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:Move()
-    for _, v in ipairs(Engine.Events) do
+func _G.Engine:Move()
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "Move") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:Damage()
-    for _, v in ipairs(Engine.Events) do
+func _G.Engine:Damage()
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "Damage") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:Death()
-    for _, v in ipairs(Engine.Events) do
+func _G.Engine:Death()
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "Death") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:Spawn()
-    for _, v in ipairs(Engine.Events) do
+func _G.Engine:Spawn()
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Name == "Spawn") then
-            v.Func();
+            v.Func()
         end
     end
 end
 
-func Engine:Init()
-    PrintLn('Registering engine...');
+func _G.Engine:Init()
+    PrintLn('Registering engine...')
 
-    Engine.Name = "Xplicit";
-    Engine.Description = "GameNgine";
-    Engine.Version = "1.0.2";
+    _G.Engine.Name = "Xplicit"
+    _G.Engine.Description = "GameNgine"
+    _G.Engine.Version = "1.0.2"
 
     # Given by server to initialize UI and stuff...
-    Engine.AutorunClient = "xasset://autorun-client.lua";
+    _G.Engine.AutorunClient = "xasset://autorun-client.lua"
 end
 
-func Engine:Connect(Name, Id, Func)
-    PrintLn('Registering event...');
+func _G.Engine:Connect(Name, id, Func)
+    PrintLn('Registering event...')
     
-    table.insert(Engine.Events, {
+    table.insert(_G.Engine.Events, {
         Name = Name,
         Func = Func,
-	    Id = id,
-    });
+	Id = Id,
+    })
 
-    Engine.Counter = Engine.Counter + 1;
+    _G.Engine.Counter = _G.Engine.Counter + 1
 
-    PrintLn('Registered!');
+    PrintLn('Registered!')
 end
 
-func Engine:Disconnect(Id)
-    PrintLn('Disconnecting event...');
+func _G.Engine:Disconnect(Id)
+    PrintLn('Disconnecting event...')
     
-    for _, v in ipairs(Engine.Events) do
+    for _, v in ipairs(_G.Engine.Events) do
         if (v.Id == Id) then
-            v = {};
+            v = {}
 
-            Engine.Counter = Engine.Counter - 1;
+            _G.Engine.Counter = _G.Engine.Counter - 1
 
-            PrintLn('Event Disconnected...');
+            PrintLn('Event Disconnected...')
         end
     end
 end
 
-Engine:Init();
+_G.Engine:Init()
 
 Engine.INVALID = 0;
 Engine.SCRIPT = 1;
@@ -165,4 +163,4 @@ Engine.EXPLOSION = 9;
 Engine.SKYBOX = 10;
 Engine.UNLOAD = 11;
 Engine.LOAD = 12;
-Engine.COUNT;
+Engine.COUNT = 13;
