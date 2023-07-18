22d1eb2f1d92db85a0f3bf8196dd3f880e5934a2
Amlal El Mahrouss <amlalelmahrouss@icloud.com>
2023-07-18 12:10:13 +0000

    Update CommonEngine.h Remove unused enum fields.

--- XplicitNgin/Source/CommonEngine.h
+++ XplicitNgin/Source/CommonEngine.h
@@ -33,9 +33,7 @@ namespace Xplicit
 		COMPONENT_ID_SMOKE = 7,
 		COMPONENT_ID_FORCEFIELD = 8,
 		COMPONENT_ID_EXPLOSION = 9,
-		COMPONENT_ID_SKYBOX = 10,
-		COMPONENT_ID_UNLOAD = 11,
-		COMPONENT_ID_LOAD = 12,
+		COMPONENT_ID_SKYBOX = 10,	
 		COMPONENT_ID_COUNT,
 		// NOTE: add more here, don't forget you can easily mod the engine with CLua.
 	};
