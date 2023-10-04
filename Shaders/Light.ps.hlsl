118d7f732c0c4b280afc4225d0b1ab068b8d5548
Amlal <amlal@xpx.com>
2023-10-04 21:56:25 +0200

    XAGE: Renderer: Implementing phong based light.
    Signed-off-by: Amlal <amlal@xpx.com>

--- Shaders/Light.ps.hlsl
+++ Shaders/Light.ps.hlsl
@@ -29,32 +29,38 @@ cbuffer LIGHT
 
 float4 PS(PIXEL input) : SV_TARGET
 {
-    float4 textureColor;
-    float3 lightDir;
-    float lightIntensity;
-    float4 color;
-    float3 reflection;
-    float4 specular;
+ // ambient lighting (global illuminance)
+    float3 ambient = float3(0.6, 0.6, 0.6); // color - grey
 
-    textureColor = gShaderTexture.Sample(SAMPLE_TYPE, input.TEXTURE);
-    textureColor *= gShaderTexture2.Sample(SAMPLE_TYPE, input.TEXTURE);
+    // diffuse (lambertian) lighting
+    // lightColor, lightSource, normal, diffuseStrength
+    float3 normal = input.NORMAL;
+    float3 lightColor = DIFFUSE_COLOR; // color - white
+    float3 lightSource = input.POSITION.xyz - DIRECTION; // coord - (1, 0, 0)
+    float diffuseStrength = max(0.1, dot(lightSource, normal));
+    float3 diffuse = diffuseStrength * lightColor;
+
+    // specular light
+    // lightColor, lightSource, normal, specularStrength, viewSource
+    float3 cameraSource = input.POSITION.xyz;
+    float3 viewSource = normalize(cameraSource);
+    float3 reflectSource = normalize(reflect(-lightSource, normal));
+    float specularStrength = max(0.01, dot(viewSource, reflectSource));
+    specularStrength = pow(specularStrength, SPECULAR_POWER);
+    float3 specular = specularStrength * lightColor;
+
+    // lighting = ambient + diffuse + specular
+    float3 lighting = SPECULAR_COLOR.xyz; // color - black
+    // lighting = ambient;
+    // lighting = ambient * 0.0 + diffuse;
+    // lighting = ambient * 0.0 + diffuse * 0.0 + specular;
+    lighting = ambient * .5 + ((diffuse / 6.0) * (diffuse / 4.0) + (diffuse / 2.0)) + specular * 0.5;
+
+    // color = modelColor * lighting
+    float3 modelColor = gShaderTexture.Sample(SAMPLE_TYPE, input.TEXTURE);
+    modelColor *= gShaderTexture2.Sample(SAMPLE_TYPE, input.TEXTURE);
     
-    color = AMBIENT_COLOR;
-    specular = SPECULAR_COLOR;
-    
-    lightDir = -DIRECTION;
-    lightIntensity = saturate(dot(input.NORMAL, lightDir));
+    float3 color = modelColor * lighting;
 
-    if (lightIntensity > 0.0f)
-    {
-        color += (DIFFUSE_COLOR * lightIntensity);
-        color = saturate(color);
-        reflection = normalize(2.0f * lightIntensity * input.NORMAL - lightDir);
-        specular = pow(saturate(dot(reflection, input.VIEW_DIR)), SPECULAR_POWER);
-    }
-
-    color = color * textureColor;
-    color = saturate(color * specular);
-
-    return color;
+    return float4(color, 1.0);
 }
