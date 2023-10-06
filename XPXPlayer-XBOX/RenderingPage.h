16eb20116fe89f4e42b9dc81fa42ecfebe8af5c7
Amlal <amlal@xpx.com>
2023-10-06 19:32:16 +0200

    XAGE: UWP: Getting it to UWP.
    Signed-off-by: Amlal <amlal@xpx.com>

---
+++ XPXPlayer-XBOX/RenderingPage.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "RenderingPage.g.h"
+
+namespace winrt::XPXPlayerXBOX::implementation
+{
+    struct RenderingPage : RenderingPageT<RenderingPage>
+    {
+        RenderingPage() 
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::XPXPlayerXBOX::factory_implementation
+{
+    struct RenderingPage : RenderingPageT<RenderingPage, implementation::RenderingPage>
+    {
+    };
+}
